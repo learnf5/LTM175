943abaf684028a5ef13588ddc9b504722d1172fb
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-02 22:50:26 -0400

    Create 20-LTM175-C1A02-LB.sh

---
+++ 20-LTM175-C1A02-LB.sh
@@ -0,0 +1,12 @@
+# download config script from GitHub, copy to bigip1 and run on bigip1
+curl --silent https://raw.githubusercontent.com/learnf5/ltm/main/iRules_v16.1_ClassSetup.sh -o /tmp/iRules_v16.1_ClassSetup.sh
+sudo scp /tmp/iRules_v16.1_ClassSetup.sh 192.168.1.31:/shared/tmp/",
+sudo ssh root@192.168.1.31 source /shared/tmp/iRules_v16.1_ClassSetup.sh",
+
+# change the DNS servers
+sudo sed --in-place -e s/8.8.8.8/10.10.17.53/ -e /8.8.4.4/d /etc/netplan/01-config.yaml
+sudo netplan apply
+
+# confirm bigip1 is active again 
+sleep 5
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
