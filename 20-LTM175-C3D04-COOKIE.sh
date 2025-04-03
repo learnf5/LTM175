55bc7c9078039b74105c76f05ba72957ebc517dd
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-02 22:56:19 -0400

    Create 20-LTM175-C3D04-COOKIE.sh

---
+++ 20-LTM175-C3D04-COOKIE.sh
@@ -0,0 +1,11 @@
+# set scf name
+SCF=cookper.scf
+
+# download config from GitHub, copy to bigip1 and load/merge onto bigip1
+curl --silent https://raw.githubusercontent.com/learnf5/ltm/main/$SCF --output /tmp/$SCF
+sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
+sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
+
+# confirm bigip1 is active again 
+sleep 5
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
