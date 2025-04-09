6d0d31ddc433c639460f35cbead5290d29be17e9
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-09 10:47:49 -0400

    Update 10-LTM175.sh

--- 10-LTM175.sh
+++ 10-LTM175.sh
@@ -1,5 +1,2 @@
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
-
-# confirm bigiq1 is active
-for i in {1..12}; do [ "$(sudo ssh root@192.168.1.51 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
