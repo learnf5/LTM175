05f94f8e68db4ea87d23686dea0c9ac231904ef3
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-01 13:43:13 -0400

    Update 20-LTM175-C1A02-LB.sh

--- 20-LTM175-C1A02-LB.sh
+++ 20-LTM175-C1A02-LB.sh
@@ -6,7 +6,3 @@ sudo ssh root@192.168.1.31 source /shared/tmp/iRules_v16.1_ClassSetup.sh
 # change the DNS servers
 sudo sed --in-place -e s/8.8.8.8/10.10.17.53/ -e /8.8.4.4/d /etc/netplan/01-config.yaml
 sudo netplan apply
-
-# confirm bigip1 is active again 
-sleep 5
-for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
