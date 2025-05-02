0cc7cd7af13f92e278aa8125902fbfe2271b242f
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-02 00:25:56 -0400

    Update 20-LTM175-C3D02-MATCH.sh

--- 20-LTM175-C3D02-MATCH.sh
+++ 20-LTM175-C3D02-MATCH.sh
@@ -5,3 +5,10 @@ SCF=MATCH.scf
 curl --silent https://raw.githubusercontent.com/learnf5/ltm/main/$SCF --output /tmp/$SCF
 sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
+
+# also download these SCF files from GitHub and copy to bigip1, but do not load/merge them
+MORE_SCF="MONITOR UNIVERSAL"
+for SCF in MORE_SCF; do
+  curl --silent https://raw.githubusercontent.com/learnf5/ltm/main/$SCF --output /tmp/$SCF
+  sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
+done
