8d9bfa3f82ae65f1b4ac4087bf68a825fe3066ec
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-02 00:21:41 -0400

    Update 20-LTM175-C4C09-HEADER.sh

--- 20-LTM175-C4C09-HEADER.sh
+++ 20-LTM175-C4C09-HEADER.sh
@@ -5,7 +5,3 @@ SCF=MONITOR.scf
 curl --silent https://raw.githubusercontent.com/learnf5/ltm/main/$SCF --output /tmp/$SCF
 sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
-
-# confirm bigip1 is active again 
-sleep 5
-for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
