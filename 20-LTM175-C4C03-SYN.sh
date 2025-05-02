afd0c85b9f7098537ce5f72cb159e54125c17953
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-02 00:31:49 -0400

    Update 20-LTM175-C4C03-SYN.sh

--- 20-LTM175-C4C03-SYN.sh
+++ 20-LTM175-C4C03-SYN.sh
@@ -5,3 +5,8 @@ SCF=SYN.scf
 curl --silent https://raw.githubusercontent.com/learnf5/ltm/main/$SCF --output /tmp/$SCF
 sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
+
+# also download this SCF file from GitHub and copy to bigip1, but do not load/merge it
+SCF="MATCH"
+curl --silent https://raw.githubusercontent.com/learnf5/ltm/main/$SCF --output /tmp/$SCF
+sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
