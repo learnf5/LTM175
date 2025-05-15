071f316802594d9d44579946901f698fa0cfb446
djlamey <76072289+djlamey@users.noreply.github.com>
2025-05-15 15:29:51 -0400

    Update 20-LTM175-C3C11-STREAM.sh
    added line to download a copy & paste text file to JUMP

--- 20-LTM175-C3C11-STREAM.sh
+++ 20-LTM175-C3C11-STREAM.sh
@@ -3,3 +3,6 @@ SCF=MONITOR.scf
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$SCF --output /tmp/$SCF
 sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
+
+# download a copy_paste file from Github and place it on the Desktop of Student Workstation
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/STREAM_copy_paste.txt --output /home/student/Desktop/Copy_Paste.txt
