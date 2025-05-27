7b8be94f08ecad8205aee12daf3c33fb63445314
djlamey <76072289+djlamey@users.noreply.github.com>
2025-05-27 14:07:37 -0400

    Update 20-LTM175-C3D06-MATCH.sh
    added line to download the copy and paste txt file

--- 20-LTM175-C3D06-MATCH.sh
+++ 20-LTM175-C3D06-MATCH.sh
@@ -4,6 +4,9 @@ curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$SCF --o
 sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
 
+# download a copy_paste file from Github and place it on the Desktop of Student Workstation
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/MATCH_copy_paste.txt --output /home/student/Desktop/Copy_Paste.txt
+
 # also download these SCF files from GitHub and copy to bigip1, but do not load/merge them
 MORE_SCF="MONITOR.scf UNIVERSAL.scf"
 for SCF in $MORE_SCF; do
